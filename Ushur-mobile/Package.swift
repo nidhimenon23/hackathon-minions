914800bf9580bf6f709189d91d709a76077fbc0c
imaya-triffy <nidhi.menon@ushur.com>
2024-04-18 00:37:47 +0530

    Added  Ushur Package

---
+++ Ushur-mobile/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Ushur-mobile",
+    platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Ushur-mobile",
+            targets: ["Ushur-mobile"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Ushur-mobile"),
+        .testTarget(
+            name: "Ushur-mobileTests",
+            dependencies: ["Ushur-mobile"]),
+    ]
+)
---
+++ Ushur-mobile/Tests/Ushur-mobileTests/Ushur_mobileTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Ushur_mobile
+
+final class Ushur_mobileTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
